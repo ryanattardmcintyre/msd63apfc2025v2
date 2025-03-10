c1946147426ac8b4021cd55c36113c1b37be4692
Ryan Attard <ryanattard@gmail.com>
2025-03-10 10:00:07 +0100

    Cache part 1

---
+++ Models/Menu.cs
@@ -0,0 +1,11 @@
+﻿namespace PFCWebApplication.Models
+{
+    public class Menu
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public int Order { get; set; }
+
+    }
+}
---
+++ Repositories/RedisRepository.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using PFCWebApplication.Models;
+using StackExchange.Redis;
+
+namespace PFCWebApplication.Repositories
+{
+    //1. Redis cache library StackExchange.Redis
+    //2. Newtonsoft.Json
+    public class RedisRepository
+    {
+        IDatabase db;
+        public RedisRepository(string endPoint, string user, string password) {
+          
+            ConfigurationOptions conf = new ConfigurationOptions
+            {
+                EndPoints = { endPoint },
+                User = user,
+                Password = password
+            };
+
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(conf);
+            db = redis.GetDatabase();
+        }
+
+        public void Add(Menu m)
+        {
+            var myList = GetAll(); //getting the list of menus already stored in cache
+            myList.Add(m);
+            var myMenus = JsonConvert.SerializeObject(myList);
+
+            db.StringSet("menu", myMenus); //overwriting the menu key in the cache
+        }
+        public List<Menu> GetAll()
+        {
+            List<Menu> myList = new List<Menu>();
+            var myMenus = db.StringGet("menu");
+            if (myMenus.HasValue)
+            {
+                //deserialize means = convert from a string into a List<Menu>
+                myList = JsonConvert.DeserializeObject<List<Menu>>(myMenus);
+            }
+            return myList.OrderBy(x=>x.Order).ToList();
+        }
+    }
+}
